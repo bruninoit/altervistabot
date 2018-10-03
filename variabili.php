db01891fcc3edaa011eedf0fb4eb9cc24f549970
davtur19 <dav.tur19@gmail.com>
2018-10-03 12:58:06 +0200

    Check variables

--- variabili.php
+++ variabili.php
@@ -1,5 +1,8 @@
 <?php
 
+$canale = false;
+$editato = false;
+
 if ($config['funziona_nei_canali'] and isset($update["channel_post"])) {
     $update["message"] = $update["channel_post"];
     $canale = true;
@@ -17,32 +20,32 @@ if ($config['funziona_messaggi_modificati_canali'] and isset($update["edited_cha
 }
 
 if (isset($update["message"])) {
-    $chatID = $update["message"]["chat"]["id"];
-    $userID = $update["message"]["from"]["id"];
-    $msg = $update["message"]["text"];
-    $username = $update["message"]["from"]["username"];
-    $nome = $update["message"]["from"]["first_name"];
-    $cognome = $update["message"]["from"]["last_name"];
-    $voice = $update["message"]["voice"]["file_id"];
-    $photo = $update["message"]["photo"][0]["file_id"];
-    $document = $update["message"]["document"]["file_id"];
-    $audio = $update["message"]["audio"]["file_id"];
-    $sticker = $update["message"]["sticker"]["file_id"];
+    $chatID = isset($update["message"]["chat"]["id"]) ? $update["message"]["chat"]["id"] : false;
+    $userID = isset($update["message"]["from"]["id"]) ? $update["message"]["from"]["id"] : false;
+    $msg = isset($update["message"]["text"]) ? $update["message"]["text"] : false;
+    $username = isset($update["message"]["from"]["username"]) ? $update["message"]["from"]["username"] : false;
+    $nome = isset($update["message"]["from"]["first_name"]) ? $update["message"]["from"]["first_name"] : false;
+    $cognome = isset($update["message"]["from"]["last_name"]) ? $update["message"]["from"]["last_name"] : false;
+    $voice = isset($update["message"]["voice"]["file_id"]) ? $update["message"]["voice"]["file_id"] : false;
+    $photo = isset($update["message"]["photo"][0]["file_id"]) ? $update["message"]["photo"][0]["file_id"] : false;
+    $document = isset($update["message"]["document"]["file_id"]) ? $update["message"]["document"]["file_id"] : false;
+    $audio = isset($update["message"]["audio"]["file_id"]) ? $update["message"]["audio"]["file_id"] : false;
+    $sticker = isset($update["message"]["sticker"]["file_id"]) ? $update["message"]["sticker"]["file_id"] : false;
     if ($chatID < 0) {
-        $titolo = $update["message"]["chat"]["title"];
-        $usernamechat = $update["message"]["chat"]["username"];
+        $titolo = isset($update["message"]["chat"]["title"]) ? $update["message"]["chat"]["title"] : false;
+        $usernamechat = isset($update["message"]["chat"]["username"]) ? $update["message"]["chat"]["username"] : false;
     }
 }
 
 //tastiere inline
 if (isset($update["callback_query"])) {
-    $cbid = $update["callback_query"]["id"];
-    $cbdata = $update["callback_query"]["data"];
-    $cbmid = $update["callback_query"]["message"]["message_id"];
-    $chatID = $update["callback_query"]["message"]["chat"]["id"];
-    $userID = $update["callback_query"]["from"]["id"];
-    $nome = $update["callback_query"]["from"]["first_name"];
-    $cognome = $update["callback_query"]["from"]["last_name"];
-    $username = $update["callback_query"]["from"]["username"];
+    $cbid = isset($update["callback_query"]["id"]) ? $update["callback_query"]["id"] : false;
+    $cbdata = isset($update["callback_query"]["data"]) ? $update["callback_query"]["data"] : false;
+    $cbmid = isset($update["callback_query"]["message"]["message_id"]) ? $update["callback_query"]["message"]["message_id"] : false;
+    $chatID = isset($update["callback_query"]["message"]["chat"]["id"]) ? $update["callback_query"]["message"]["chat"]["id"] : false;
+    $userID = isset($update["callback_query"]["from"]["id"]) ? $update["callback_query"]["from"]["id"] : false;
+    $nome = isset($update["callback_query"]["from"]["first_name"]) ? $update["callback_query"]["from"]["first_name"] : false;
+    $cognome = isset($update["callback_query"]["from"]["last_name"]) ? $update["callback_query"]["from"]["last_name"] : false;
+    $username = isset($update["callback_query"]["from"]["username"]) ? $update["callback_query"]["from"]["username"] : false;
     $msg = $cbdata;
 }
